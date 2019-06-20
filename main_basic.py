5705191c1bf0fd2e06044e4d828af972b9cf9e7d
englerfa <englerfa@student.ethz.ch>
2019-06-20 17:34:28 +0200

    rearranged tests

--- main_basic.py
+++ main_basic.py
@@ -1,14 +1,58 @@
-
-
-
+'''
+Different function/method definitions to test if all of them are logged correctly.
+'''
 
 def print_text():
     print("print_text() is being called")
 
 class A:
+    balance = 5
+    class_name = "A"
+
+    def print_text(self):
+        print("print_text(self) is being called from" , self.class_name)
+
     def add_numbers(self, first, second):
-        print("add_numbers(self,first,second) is being called")
+        print("add_numbers(self,first,second) is being called from", self.class_name)
         return first + second
+
+    def add_numbers1(self, first, second, third):
+        print("add_numbers1(self, first, second, third) is being called from", self.class_name)
+        return first + second
+
+    def get_balance(self):
+        print("get_balance(self) is being called from", self.class_name)
+        return self.balance
+
+    def multiple_calls(self, first):
+        print("multiple_calls(self, first) is being called from", self.class_name)
+        print_text()
+
+    class A1:
+        class_name = "inner class A1"
+        def print_text(self):
+            print("print_text(self) is being called from", self.class_name)
+
+
+
+class B:
+    class_name = "B"
+    def swap(self, a, b):
+        print("swap(self,a,b) is being called")
+        (b, a) = (a, b)
+
+    @staticmethod
+    def static_method():
+        print("static_method() is being called")
+
+
+
+
+
+
+
+
+
 
 
 
--- main_basic_testing.py
+++ main_basic_testing.py
@@ -1,57 +1,30 @@
-# 13.06.2019
-
-import main
 import main_basic
-import main_nested
 import logging              # if we import this module, then logging is automatically activated
 
 
-# TODO catch exceptions. Print error instead.
-# TODO get all methods automatically and call all them with random parameters.
+# Call functions from module main_basic
+main_basic.print_text()
 
-
-
-
-
-# Test function/method calls
-# Module main
-#main.print_text()
-obj_A = main.A()           # create new object
+# Call class methods from module main_basic
+obj_A = main_basic.A()           # create new object
 obj_A.print_text()
 obj_A.add_numbers(3,5)
+obj_A.add_numbers(-3,-5)
+obj_A.add_numbers(first=3,second=5)
+obj_A.add_numbers(second=3,first=5)
 obj_A.add_numbers1(3,5,99)
 obj_A.get_balance()
 obj_A.multiple_calls(7)
-obj_A.swap(3,5)
 
-obj_B = obj_A.B()           # create new object
-obj_B.print_text()
+obj_A1 = obj_A.A1()              # create new object
+obj_A1.print_text()
 
-
-main.F.static_method()
-
-obj_F = main.F()            # create new object
-obj_F.arg_set(5)
+obj_B = main_basic.B()           # create new object
+obj_B.swap(3,5)
+main_basic.B.static_method()
 
 
 
-
-# Module
-main_basic.print_text()          # function
-main_basic_a = main_basic.A()       # create object/instance
-main_basic_a.add_numbers(3,5)       # method
-
-# Module
-main_nested.f0()
-
-main_basic_nested_A = main_nested.A()
-main_basic_nested_A.fA()
-
-main_basic_nested_B = main_nested.B()
-main_basic_nested_B.fB()
-
-main_basic_nested_B_C = main_nested.B.C()
-main_basic_nested_B_C.fC()
 
 
 

d52e770670c6d596356f60c55a5f85e85751c890
bender250 <karel.kubicek@mail.muni.cz>
2019-07-02 17:57:43 +0200

    Real aps are now almost woring
    Solved:
    * _empty
    * *args and **kwargs
    * return
    * module in module (e.g., svm in sklearn)
    * default arguments in the call
    * primitive module recusrion with depth limitation

--- autolog.py
+++ autolog.py
@@ -19,38 +19,56 @@ import inheritance
 import nested
 
 functions = []
-modules_to_log = [basic, basic2, inheritance, nested]
+modules_to_log = []
+
 
 def add_modules(list_of_modules):
-    global  modules_to_log
+    global modules_to_log
     modules_to_log = list_of_modules
+
 
 def run():
     _traverse_modules(modules_to_log)
     _execute_monkey_patching()
     print("Monkey patching done, executing the program")
 
-def _traverse(nodes):
+
+def _traverse(nodes, depth):
+    if depth < 0:
+        return
     for elem in inspect.getmembers(nodes):
         if type(elem[1]).__name__ == "function":                            # get global functions
+            mod = inspect.getmodule(elem[1])
             tup = (elem[1], mod.__name__)
+            if '<' in tup[0].__name__ or '<' in tup[1]:
+                continue  # prevent inspecting invalid paths
             functions.append(tup)
-        if type(elem[1]) == type.__class__ and elem[0] != "__class__":      # Need to exclude tup '__class__', since they are also of type 'class'
-            _traverse(elem[1])   # recursion to get nested classes
+        elif type(elem[1]) == type.__class__ and elem[0] != "__class__":      # Need to exclude tup '__class__', since they are also of type 'class'
+            _traverse(elem[1], depth-1)   # recursion to get nested classes
+        elif type(elem[1]).__name__ == "module":
+            print(f'found module elem[1].__name__')
+            traverse_mod(elem[1], depth-1)  # recursion to the module
+
 
 def _traverse_modules(mods):
-    global mod
     for mod in mods:
-        mod_path = mod.__name__.split('.')
-        if len(mod_path) > 1:
-            command = f'global {mod_path[-1]}\nfrom {".".join(mod_path[:-1])} import {mod_path[-1]}'
-        else:
-            command = f'global {mod_path[-1]}\nimport {mod_path[-1]}'
-        #print(command)
-        exec(command)
-        _traverse(mod)
+        traverse_mod(mod, 1)
 
-def _format_signature(signature):
+
+def traverse_mod(mod, depth):
+    mod_path = mod.__name__.split('.')
+    while len(mod_path) > 1:
+        command = f'global {mod_path[-1]}\nfrom {".".join(mod_path[:-1])} import {mod_path[-1]}'
+        print(command)
+        exec(command)
+        mod_path.pop()
+    command = f'global {mod_path[-1]}\nimport {mod_path[-1]}'
+    print(command)
+    exec(command)
+    _traverse(mod, depth)
+
+
+def _format_signature(signature, value=True):
     res = '('
     comma = False        # solution for post fence problem
     for sig in signature.parameters:
@@ -76,36 +94,41 @@ def _format_signature(signature):
             res += ', '
         if '**' in str(signature.parameters[sig]):
             res += '**'
+        elif '*' in str(signature.parameters[sig]):
+            res += '*'
         res += signature.parameters[sig].name
-        if not empty:
-             res += '=' + default
+        if value and not empty:
+            res += '=' + default
         comma = True
     res += ')'
     #print('resulting signature: ' + res)
     return res
 
+
 def _execute_monkey_patching():
     i = 0
     for f in functions:
-        s_name = f[1] + "." + f[0].__qualname__
+        s_name = f[1] + "." + f[0].__name__
 
         s_global = f"global f_original{str(i)}"
         s_original = f"f_original{str(i)}={s_name}"         # str(i) is needed to create for every function an individual name, otherwise it gets overwritten (point to same reference)
 
-        s_signature = str(inspect.signature(f[0]))
+        s_signature = _format_signature(inspect.signature(f[0]), value=False)
         s_signature_values = _format_signature(inspect.signature(f[0]))
 
         s_f_def = f"def f_monkey{s_signature_values}:"
 
         s_f_log = f"  print(f\"autolog fun called: {s_name}{s_signature_values}\")"
 
-        s_f_call = f"  result = f_original{str(i)}{s_signature_values}"
+        s_f_call = f"  result = f_original{str(i)}{s_signature}"
         
-        s_f_ret = "  print(result, type(result))"
+        s_f_res = "  print(result, type(result))"
+        
+        s_f_ret = "  return result"
 
         s_replace = s_name + ' = f_monkey'
 
-        s_execute = '\n'.join([s_global, s_original, s_f_def, s_f_log, s_f_call, s_f_ret, s_replace])
+        s_execute = '\n'.join([s_global, s_original, s_f_def, s_f_log, s_f_call, s_f_res, s_f_ret, s_replace])
         print(s_execute)
         exec(s_execute)
 

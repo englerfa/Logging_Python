d5d6dc4b8bcbb7f12985126a423e077c0c4db486
englerfa <englerfa@student.ethz.ch>
2019-07-10 20:56:08 +0200

    added module parallel

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import nested
 import inheritance
 import complex_types
 import libuse
+import parallel
 
 from autolog import Autolog
 
-autolog = Autolog([basic, basic2, nested, inheritance, complex_types, libuse])
+autolog = Autolog([basic, basic2, nested, inheritance, complex_types, libuse, parallel])
 autolog.run()  # by running this function, logging is activated
 
 complex_types.run()
@@ -16,3 +17,4 @@ basic.run()
 basic2.run()
 nested.run()
 inheritance.run()
+parallel.run()
